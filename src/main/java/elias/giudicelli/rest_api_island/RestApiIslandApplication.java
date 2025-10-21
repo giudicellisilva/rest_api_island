ddafe4dfecee77492861820cbced65d4252610f9
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2025-10-21 06:39:37 -0300

    initial project

---
+++ src/main/java/elias/giudicelli/rest_api_island/RestApiIslandApplication.java
@@ -0,0 +1,13 @@
+package elias.giudicelli.rest_api_island;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiIslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiIslandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elias/giudicelli/rest_api_island/RestApiIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package elias.giudicelli.rest_api_island;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiIslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
